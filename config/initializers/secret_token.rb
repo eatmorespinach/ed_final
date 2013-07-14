20606f3b5d908fd682de385f3f145e503d335225
Drew Sing <sing.drew@gmail.com>
2013-07-14 11:42:03 -0400

    initialized project, and installed rspec and capybara for testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyProject::Application.config.secret_key_base = 'a21908352a461c3bd1d48445459f7e425d49e10397b3c921d2764a72e71958b2c1b2a2f93a8d09b6c2a9b33ed51782941ee77b4eb3e7f4ae137cd10110cc7284'
