5470709487f904bd572f478a7dcf9ca32d133a0b
Drew Sing <sing.drew@gmail.com>
2013-07-16 20:46:42 -0400

    successfully added user_id to reviews table

--- db/migrate/20130717003408_add_user_to_reviews.rb
+++
@@ -1,7 +0,0 @@
-class AddUserToReviews < ActiveRecord::Migration
-  def change
-  	change_table :reviews do |t|
-      t.belongs_to :user
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716001421) do
+ActiveRecord::Schema.define(version: 20130717001946) do
 
   create_table "listings", force: true do |t|
     t.datetime "created_at"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130716001421) do
     t.integer "rating"
     t.text    "description"
     t.integer "listing_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
