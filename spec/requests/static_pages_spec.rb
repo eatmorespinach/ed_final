a68a4db34bb77e47bccbd5f958f69f13604ee13c
Eric Szeto <eszeto@gmail.com>
2013-07-19 17:35:06 -0400

    added tests for landing page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+	describe "landing page" do
+
+		visit root_path
+
+		it "should have title StayTraders" do
+			page.should have_content("StayTraders")
+		end
+
+
+
+		describe "navbar" do
+
+			it "should have non logged in nav bar" do
+				page.should have_content("Sign Up")
+				page.should have_content("Log In")
+			end
+
+		end
+	end
+
+	
+
+end
