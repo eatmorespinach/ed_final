bc20d85c8f0d347f6a620119f41bfdc756f2ac3b
Eric Szeto <eszeto@gmail.com>
2013-07-22 18:52:56 -0400

    add listing and review to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,20 @@ FactoryGirl.define do
   	email "Tedlikestodance@gmail.com"
   	password "password"
   end
+  factory :listing do
+  	title "Bitmaker"
+  	description "Description stuff"
+  	guests 3
+  	city "Toronto"
+  	country_id 1
+  	stay_length "Here for a while"
+  	in_exchange "Want money"
+  	user
+  end
+  factory :review do
+  	rating 9
+  	description "It was a pretty good place to stay"
+  	user
+  	listing
+  end
 end
