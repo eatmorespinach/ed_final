24530dc8b7db8141f84efc00cd85d4c6a9185ab5
Eric Szeto <eszeto@gmail.com>
2013-07-24 18:18:38 -0400

    add image index, upload, can use url instead

--- app/controllers/lis_images_controller.rb
+++ app/controllers/lis_images_controller.rb
@@ -13,22 +13,19 @@ before_filter :load_listing
 		@lis_image = @listing.lis_images.build(params_image)
 
 	    if @lis_image.save
-	      redirect_to listings_url(@listing), :alert => "Hosting created!"
+	      redirect_to listing_lis_images_path(@listing), :alert => "Image Added"
 	    else
-	      render :new, notice: 'Error image not uploaded'
+	      render 'new', notice: 'Error image not uploaded'
 	    end
 	end
 
-	def edit
-	end
-
-	def update
+	def destroy
 	end
 
 	private
 
 	def params_image
-    	params.require(:lis_image).permit(:file)
+    	params.require(:lis_image).permit(:file, :remote_file_url)
 	end
 
 	def load_listing
