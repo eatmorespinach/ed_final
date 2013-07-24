88bf9ed06d7f1da20d34a8cb9c28cb400f19b290
Eric Szeto <eszeto@gmail.com>
2013-07-24 14:19:28 -0400

    test for profile button on root

---
+++ spec/requests/profile_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'User edits their profile' do 
+	before(:each) do
+		# user = FactoryGirl.create(:user, password: 'password')
+		# visit new_session_path
+		# fill_in('Email', :with => user.email)
+		# fill_in('Password', :with => 'password')
+		# click_button 'Log In'
+		visit root_path
+	end
+
+	it "non logged in user shoould not see profile button" do
+		page.should_not have_content("Profile")
+	end
+
+	it "logged in user should see profile button on root" do
+		user = FactoryGirl.create(:user, password: 'password')
+		visit new_session_path
+		fill_in('Email', :with => user.email)
+		fill_in('Password', :with => 'password')
+		click_button 'Log In'
+		page.should have_content("Profile")
+	end
+	
+
+	# it "for logged in user profile page should have dit profile button" do
+	# 	clink_link 'Profile'
+	# 	expect(page).to have_link("Edit Profile")
+	# end
+
+	
+
+
+
+end
