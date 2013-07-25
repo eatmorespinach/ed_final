54373c79804fc834f7d0ec2e9b0fbceda0b77698
Drew Sing <sing.drew@gmail.com>
2013-07-25 16:26:42 -0400

    fabio commit

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
 	has_many :pro_images, dependent: :destroy
+	delegate :first_name, :last_name, :to => :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+
   has_many :reviews, dependent: :destroy
   has_many :listings, dependent: :destroy
   has_one :profile, dependent: :destroy
@@ -12,5 +13,12 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  after_save :create_profile
+
+  def create_profile
+
+    Profile.create!(user_id: self.id) if profile.blank?
+  end
 
 end
