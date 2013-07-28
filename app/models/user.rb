26f10432b2fed3772cf008a404c4ef69738522e3
Drew Sing <sing.drew@gmail.com>
2013-07-28 12:05:12 -0400

    added a capitalize callback for first and last name upon sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-
   has_many :reviews, dependent: :destroy
   has_many :listings, dependent: :destroy
   has_one :profile, dependent: :destroy
 
-  before_save {|user| user.email = email.downcase}
   validates :first_name, presence: true, length: {maximum: 30}
   validates :last_name, presence: true, length: {maximum: 30}
   validates :password, length: {minimum: 6}, :if => :password
@@ -15,10 +13,15 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
+  before_save do |user| 
+    user.email = email.downcase
+    user.first_name = first_name.capitalize
+    user.last_name = last_name.capitalize
+  end
+
   after_save :create_profile
 
   def create_profile
-
     Profile.create!(user_id: self.id) if profile.blank?
   end
 
