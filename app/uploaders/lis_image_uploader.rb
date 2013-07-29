1ab858526024e923821d6983bf5a85c73b190c12
Drew Sing <sing.drew@gmail.com>
2013-07-29 17:42:21 -0400

    images now centered

--- app/uploaders/lis_image_uploader.rb
+++ app/uploaders/lis_image_uploader.rb
@@ -33,7 +33,7 @@ class LisImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [400, 250]
+    process :resize_to_limit => [400, 250]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
