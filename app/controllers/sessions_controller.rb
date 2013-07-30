e5636a5e025ae70b8d8a29de1c4afb0372b542c0
Drew Sing <sing.drew@gmail.com>
2013-07-30 17:32:45 -0400

    fixed alert on sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
 	def destroy
 	  logout
-	  redirect_to root_url, :notice => "Logged out"
+	  redirect_to root_url, :alert => "Logged out"
 	end
 end
