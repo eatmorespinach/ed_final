c20a3a6b88851e68a79b1caa540b982e95b0c1af
Eric Szeto <eszeto@gmail.com>
2013-08-01 17:33:49 -0400

    tests for listing assets

--- spec/requests/asset_spec.rb
+++ spec/requests/asset_spec.rb
@@ -7,6 +7,7 @@ describe "assets" do
 
 	let(:user) {FactoryGirl.create(:user)}
 	let(:profile) {FactoryGirl.create(:profile, user: user)}
+	
 	before {sign_in user}
 
 	describe "profile assets" do
@@ -36,6 +37,37 @@ describe "assets" do
 			page.should_not have_content("Delete Image")
 		end
 
+	end
+
+	let(:listing) {FactoryGirl.create(:listing, user: user)}
+
+	describe "listing assets" do
+		before do
+			FactoryGirl.create(:listing, user: user)
+			click_link "Hostings"
+			click_link "Bitmaker"
+			click_link "Manage Images"
+		end
+
+		it "should show no images to delete" do
+			page.should_not have_content("Delete Image")
+		end
+
+		it "should allow correct user to upload image" do
+			click_link "Add Image"
+			fill_in "asset_remote_file_url", with: "https://lh6.googleusercontent.com/-aIo2RpDJRjg/AAAAAAAAAAI/AAAAAAAAAC8/1VoC52MxY1s/s46-c-k-no/photo.jpg"
+			click_on "Upload Image"
+			page.should have_content("Delete Image")
+			Asset.all.count.should == 1
+		end
+
+		it "should allow correct user to delete image" do
+			FactoryGirl.create(:asset, assetable_id: listing.id, assetable_type: listing.class.to_s)
+			visit listing_assets_path(listing)
+			page.should have_content("Delete Image")
+			click_link "Delete Image"
+			page.should_not have_content("Delete Image")
+		end
 	end
 
 	
