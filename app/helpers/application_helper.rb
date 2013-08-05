35e8133c8c3ec9b4c9904287841013f1a2344b88
Drew Sing <sing.drew@gmail.com>
2013-08-05 18:06:38 -0400

    added meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,15 @@ module ApplicationHelper
   	end
   end
 
+  def set_meta_tags(meta_tags)
+    base_meta = "working holiday, work and travel, travel for free, gap year"
+    if meta_tags.empty?
+      base_meta
+    else
+      meta_tags + base_meta
+    end
+  end
+
   def current_user?(user)
   	current_user == user
   end
