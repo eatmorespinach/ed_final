1a515bfcb2c173652111f6ae4b4fcbe457821e84
Eric Szeto <eszeto@gmail.com>
2013-08-07 20:27:59 -0400

    add google chart map

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -7,5 +7,20 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 		interval: false
 	});	
+	google.load("visualization", "1", {packages:["geochart"], callback:drawRegionsMap})
 
+	function drawRegionsMap() {
+
+		$.get('map.json', function(data) {
+
+			var list = new google.visualization.arrayToDataTable(data);
+
+			var options = {};
+
+			var chart = new google.visualization.GeoChart(document.getElementById('list_map'));
+			chart.draw(list, options);
+		})
+     
+	};
 });
+
