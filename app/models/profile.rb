1991929c9b418cceb195becfef49b8766e6c3519
Eric Szeto <eszeto@gmail.com>
2013-08-12 17:52:51 -0400

    revert

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -14,10 +14,6 @@ class Profile < ActiveRecord::Base
 		count(group: 'nationality').to_a.unshift(['Nationality', 'Users'])
 	end
 
-	def find_active
-		self.assets.each do |asset|
-			return asset if asset.active?
-		end
-	end
+	
 
 end
